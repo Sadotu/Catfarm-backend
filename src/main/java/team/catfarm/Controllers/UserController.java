e55740f8aba4378f54c17a664580eeb819cc6b58
Nick <nickdekok.nl@gmail.com>
2023-05-02 15:07:44 +0200

    created getUserPublicInfoByEmail for volunteer page

--- src/main/java/team/catfarm/Controllers/UserController.java
+++ src/main/java/team/catfarm/Controllers/UserController.java
@@ -1,5 +1,6 @@
 package team.catfarm.Controllers;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,13 +8,13 @@ import org.springframework.web.bind.annotation.*;
 import team.catfarm.Exceptions.UserAlreadyExistsException;
 import team.catfarm.Exceptions.UserNotFoundException;
 import team.catfarm.Models.User;
+import team.catfarm.Models.UserPublic;
 import team.catfarm.Services.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    @Autowired
     private final UserService userService;
 
     public UserController(UserService userService) {
@@ -30,6 +31,14 @@ public class UserController {
     public ResponseEntity<User> getUserByEmail(@PathVariable String email) throws UserNotFoundException {
         User user = userService.getUserByEmail(email);
         return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/{email}/public")
+    public ResponseEntity<UserPublic> getUserPublicInfoByEmail(@PathVariable String email) throws UserNotFoundException {
+        User user = userService.getUserByEmail(email);
+        UserPublic userPublic = new UserPublic();
+        BeanUtils.copyProperties(user, userPublic, "password", "newsletter");
+        return ResponseEntity.ok(userPublic);
     }
 
     @PutMapping("/{email}")
